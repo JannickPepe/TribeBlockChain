5355beb1fead67b6df3206f9854627035242065e
Jannick Pedersen <jannickpedersen@live.dk>
2024-09-23 21:51:03 +0200

    Implemented Latest Posts section and made FeaturesCard dynamic with colors

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -4,6 +4,7 @@ import Cone from "../../public/assets/images/cone.png";
 import Icosahedron from "../../public/assets/images/icosahedron.png";
 import { twMerge } from "tailwind-merge";
 import LearnMoreButton from "../components/LearnMoreButton";
+import Card from "../components/Card";
 
 const cardData = [
     {
@@ -45,21 +46,7 @@ export const FeaturesCards = () => {
                 <div className="mt-36 lg:mt-48 flex">
                     <div className="flex flex-none gap-8">
                         {cardData.map(({image, title, description, color}) => (
-                            <div className="relative z-0 p-8 md:p-10 max-w-xs md:max-w-md group" key={title}>
-                                <div className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300", 
-                                    color === "lime" && "bg-lime-500", 
-                                    color === "cyan" && "bg-cyan-500",  
-                                    color === "violet" && "bg-violet-500")}
-                                >
-                                </div>
-                                <div className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 group-hover:bg-fuchsia-400", 
-                                    color === "lime" && "bg-lime-500 group-hover:bg-lime-400", 
-                                    color === "cyan" && "bg-cyan-500 group-hover:bg-cyan-400",  
-                                    color === "violet" && "bg-violet-500 group-hover:bg-violet-400")}
-                                >
-                                </div>
-                                
-                                <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+                            <Card className="max-w-xs md:max-w-md" key={title} color={color}>
                                 <div className="flex justify-center -mt-28">
                                     <div className="inline-flex relative">
                                         <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
@@ -73,22 +60,7 @@ export const FeaturesCards = () => {
                                 <p className="text-lg text-zinc-400 mt-4">
                                     {description}
                                 </p>
-
-                                <div className="flex justify-between mt-12">
-                                    <LearnMoreButton color={color} >Learn More</LearnMoreButton>
-                                    <svg 
-                                        xmlns="http://www.w3.org/2000/svg" 
-                                        fill="none" 
-                                        viewBox="0 0 24 24" 
-                                        strokeWidth={2} 
-                                        stroke="currentColor" 
-                                        className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                                    </svg>
-
-                                </div>
-                            </div>
+                            </Card>
                         ))}
                     </div>
                 </div>
---
+++ src/sections/LatestPosts.tsx
@@ -0,0 +1,73 @@
+import type { CollectionEntry } from "astro:content"
+import Card from "../components/Card"
+import { getPostColorFromCategory } from "../utils/postUtils";
+import { Tag } from "../components/Tag";
+import { CutCornerButton } from "../components/CutCornerButton";
+import { twMerge } from "tailwind-merge";
+
+export const LatestPosts = (props: { latestPosts: CollectionEntry<'blog'>[] }) => {
+
+    const { latestPosts } = props;
+
+    return (
+        <section className="py-60">
+            <div className="container">
+
+                <div className="max-w-3xl mx-auto">
+                    <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">Your portal to everything BlockChain</h2>
+                    <p className="text-xl lg:2xl text-center text-zinc-400 mt-8">
+                        Keep up with the newest trends, updates and insights in the BlockChain world, updated weekly
+                    </p>
+                </div>
+            
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16 md:mt-28">
+                    <div className="flex flex-col gap-8">
+                        {latestPosts.map(({ data: { title, description, category } }, postIndex) => (
+                            <Card 
+                                key={postIndex} 
+                                color={getPostColorFromCategory(category)} 
+                                buttonText="Read More"
+                                className={twMerge((postIndex === 1 || postIndex === 3) && "md:hidden")}
+                            >
+                                <Tag color={getPostColorFromCategory(category)}>
+                                    {category}
+                                </Tag>
+                                <h3 className="font-heading font-black text-3xl mt-3">
+                                    {title}
+                                </h3>
+                                <p className="text-lg text-zinc-400 mt-6">
+                                    {description}
+                                </p>
+                            </Card>
+                        ))}
+                    </div>
+
+                    <div className="hidden md:flex flex-col gap-8 mt-16">
+                        {latestPosts.map(({ data: { title, description, category } }, postIndex) => (
+                            <Card 
+                                key={postIndex} 
+                                color={getPostColorFromCategory(category)} 
+                                buttonText="Read More"
+                                className={twMerge((postIndex === 0 || postIndex ===2 ) && "md:hidden")}
+                            >
+                                <Tag color={getPostColorFromCategory(category)}>
+                                    {category}
+                                </Tag>
+                                <h3 className="font-heading font-black text-3xl mt-3">
+                                    {title}
+                                </h3>
+                                <p className="text-lg text-zinc-400 mt-6">
+                                    {description}
+                                </p>
+                            </Card>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="flex justify-center mt-48 md:mt-32">
+                    <CutCornerButton>Read the Blog</CutCornerButton>
+                </div>
+            </div>
+        </section>
+    )
+}
