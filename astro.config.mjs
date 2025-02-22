4e76579cb50d6c26bea4cefc11e029f782c7750b
Jannick Pedersen <jannickpedersen@live.dk>
2025-02-22 20:24:13 +0100

    change adapter to netflity in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import db from "@astrojs/db";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(), 
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react(), db()],
