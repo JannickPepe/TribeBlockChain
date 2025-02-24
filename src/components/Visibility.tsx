df17ccd7fc8d46f095d0551c12684e96c6df8bf6
Jannick Pedersen <jannickpedersen@live.dk>
2025-02-24 14:52:47 +0100

    Setup new actions

---
+++ src/components/Visibility.tsx
@@ -0,0 +1,7 @@
+import type { AstroClientDirectives } from "astro";
+import type { TImagePost } from "../lib/infer-types";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import type { User } from "better-auth";
+import { actions } from "astro:actions";
+
